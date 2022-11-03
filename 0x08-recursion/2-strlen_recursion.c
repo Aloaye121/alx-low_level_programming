fa4f75ffba6beb9916acdfd02ff54dd8254ef48a
Aloaye121 <105248297+Aloaye121@users.noreply.github.com>
2022-11-03 23:41:12 +0100

    Create 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of string
+ * @s: inputed string
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion((s + 1)));
+	}
+	else
+	{
+		return (0);
+	}
+}
